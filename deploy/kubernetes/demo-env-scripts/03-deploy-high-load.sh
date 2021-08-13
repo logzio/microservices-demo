fa043b38efdcf0442391592c8128a24da714f9c8
Simon Fisher <simfish85@googlemail.com>
2021-08-13 16:19:48 +0100

    Tweaked load test volumes to show increased order rates

--- deploy/kubernetes/demo-env-scripts/03-deploy-high-load.sh
+++ deploy/kubernetes/demo-env-scripts/03-deploy-high-load.sh
@@ -4,6 +4,6 @@ echo "Scaling up load"
 
 URL=http://$(kubectl get services -n sock-shop front-end -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 
-kubectl patch configmap/loadtest-configmap -n loadtest --type merge -p '{"data":{"TARGET_HOST":"'"$URL"'","CLIENTS":"15","HATCH_RATE":"15","NUM_REQUEST":"20"}}'
+kubectl patch configmap/loadtest-configmap -n loadtest --type merge -p '{"data":{"TARGET_HOST":"'"$URL"'","CLIENTS":"4","HATCH_RATE":"4","NUM_REQUEST":"80"}}'
 
 kubectl rollout restart deployment load-test -n loadtest
--- deploy/kubernetes/demo-env-scripts/04-deploy-normal-load.sh
+++ deploy/kubernetes/demo-env-scripts/04-deploy-normal-load.sh
@@ -4,6 +4,6 @@ echo "Scaling down load"
 
 URL=http://$(kubectl get services -n sock-shop front-end -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 
-kubectl patch configmap/loadtest-configmap -n loadtest --type merge -p '{"data":{"TARGET_HOST":"'"$URL"'","CLIENTS":"2","HATCH_RATE":"2","NUM_REQUEST":"8"}}'
+kubectl patch configmap/loadtest-configmap -n loadtest --type merge -p '{"data":{"TARGET_HOST":"'"$URL"'","CLIENTS":"1","HATCH_RATE":"1","NUM_REQUEST":"8"}}'
 
 kubectl rollout restart deployment load-test -n loadtest
